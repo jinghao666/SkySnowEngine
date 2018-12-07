2381de82ae5939609cc68d7424afddee7abdb131
liuqian8 <liuqian5356@qq.com>
2018-12-07 16:34:45 +0800

    add

--- SourceCode/SkySnow/Graphics/Technique.cpp
+++ SourceCode/SkySnow/Graphics/Technique.cpp
@@ -15,17 +15,473 @@ Pass::~Pass()
 
 void Pass::SetBlendMode(BlendMode mode)
 {
+    blendMode_ = mode;
+}
 
+void Pass::SetCullMode(CullMode mode)
+{
+    cullMode_ = mode;
+}
+
+void Pass::SetDepthTestMode(CompareMode mode)
+{
+    depthTestMode_ = mode;
+}
+
+void Pass::SetLightingMode(PassLightingMode mode)
+{
+    lightingMode_ = mode;
+}
+
+void Pass::SetDepthWrite(bool enable)
+{
+    depthWrite_ = enable;
+}
+
+void Pass::SetAlphaToCoverage(bool enable)
+{
+    alphaToCoverage_ = enable;
 }
 
 
-Technique::Technique(Context *context):
-    Object(context)
+void Pass::SetIsDesktop(bool enable)
+{
+    isDesktop_ = enable;
+}
+
+void Pass::SetVertexShader(const string& name)
+{
+    vertexShaderName_ = name;
+    ReleaseShaders();
+}
+
+void Pass::SetPixelShader(const string& name)
+{
+    pixelShaderName_ = name;
+    ReleaseShaders();
+}
+
+void Pass::SetVertexShaderDefines(const string& defines)
+{
+    vertexShaderDefines_ = defines;
+    ReleaseShaders();
+}
+
+void Pass::SetPixelShaderDefines(const string& defines)
+{
+    pixelShaderDefines_ = defines;
+    ReleaseShaders();
+}
+
+void Pass::SetVertexShaderDefineExcludes(const string& excludes)
+{
+    vertexShaderDefineExcludes_ = excludes;
+    ReleaseShaders();
+}
+
+void Pass::SetPixelShaderDefineExcludes(const string& excludes)
+{
+    pixelShaderDefineExcludes_ = excludes;
+    ReleaseShaders();
+}
+
+void Pass::ReleaseShaders()
+{
+    vertexShaders_.clear();
+    pixelShaders_.clear();
+    extraVertexShaders_.clear();
+    extraPixelShaders_.clear();
+}
+
+void Pass::MarkShadersLoaded(unsigned frameNumber)
+{
+    shadersLoadedFrameNumber_ = frameNumber;
+}
+
+string Pass::GetEffectiveVertexShaderDefines() const
+{
+    // Prefer to return just the original defines if possible
+//    if (vertexShaderDefineExcludes_.empty())
+//        return vertexShaderDefines_;
+//
+//    vector<string> vsDefines = vertexShaderDefines_.Split(' ');
+//    vector<string> vsDefines = vertexShaderDefines_.Split(' ');
+//    vector<string> vsExcludes = vertexShaderDefineExcludes_.Split(' ');
+////    for (unsigned i = 0; i < vsExcludes.Size(); ++i)
+//    for (unsigned i = 0; i < vsExcludes.size(); ++i)
+////        vsDefines.remove(vsExcludes[i]);
+//        vsDefines.erase(vsExcludes[i]);
+//
+//    return string::Joined(vsDefines, " ");
+    return NULL;
+}
+
+string Pass::GetEffectivePixelShaderDefines() const
+{
+    // Prefer to return just the original defines if possible
+//    if (pixelShaderDefineExcludes_.empty())
+//        return pixelShaderDefines_;
+//
+//    vector<string> psDefines = pixelShaderDefines_.Split(' ');
+//    vector<string> psDefines = pixelShaderDefines_.substr(' ');
+//    vector<string> psExcludes = pixelShaderDefineExcludes_.Split(' ');
+//    for (unsigned i = 0; i < psExcludes.size(); ++i)
+//        psDefines.Remove(psExcludes[i]);
+
+//    return string::Joined(psDefines, " ");
+    return NULL;
+}
+
+vector<ShaderVariation>& Pass::GetVertexShaders(const string& extraDefinesHash)
+{
+    // If empty hash, return the base shaders
+    if (!extraDefinesHash.data())
+        return vertexShaders_;
+    else
+        return extraVertexShaders_[extraDefinesHash];
+}
+
+vector<ShaderVariation>& Pass::GetPixelShaders(const string& extraDefinesHash)
+{
+    if (!extraDefinesHash.data())
+        return pixelShaders_;
+    else
+        return extraPixelShaders_[extraDefinesHash];
+}
+
+
+
+unsigned Technique::basePassIndex = 0;
+unsigned Technique::alphaPassIndex = 0;
+unsigned Technique::materialPassIndex = 0;
+unsigned Technique::deferredPassIndex = 0;
+unsigned Technique::lightPassIndex = 0;
+unsigned Technique::litBasePassIndex = 0;
+unsigned Technique::litAlphaPassIndex = 0;
+unsigned Technique::shadowPassIndex = 0;
+
+SN_HashMap<string, unsigned> Technique::passIndices;
+
+Technique::Technique(Context* context) :
+        Object(context),
+        isDesktop_(false)
 {
 
 }
 
 Technique::~Technique()
 {
+}
 
+void Technique::RegisterObject(Context* context)
+{
+//    context->RegisterFactory<Technique>();
+}
+
+bool Technique::BeginLoad(File& source)
+{
+    passes_.clear();
+    cloneTechniques_.clear();
+
+//    SetMemoryUse(sizeof(Technique));
+//
+//    SharedPtr<XMLFile> xml(new XMLFile(context_));
+//    if (!xml->Load(source))
+//        return false;
+//
+//    XMLElement rootElem = xml->GetRoot();
+//    if (rootElem.HasAttribute("desktop"))
+//        isDesktop_ = rootElem.GetBool("desktop");
+//
+//    String globalVS = rootElem.GetAttribute("vs");
+//    String globalPS = rootElem.GetAttribute("ps");
+//    String globalVSDefines = rootElem.GetAttribute("vsdefines");
+//    String globalPSDefines = rootElem.GetAttribute("psdefines");
+//    // End with space so that the pass-specific defines can be appended
+//    if (!globalVSDefines.Empty())
+//        globalVSDefines += ' ';
+//    if (!globalPSDefines.Empty())
+//        globalPSDefines += ' ';
+//
+//    XMLElement passElem = rootElem.GetChild("pass");
+//    while (passElem)
+//    {
+//        if (passElem.HasAttribute("name"))
+//        {
+//            Pass* newPass = CreatePass(passElem.GetAttribute("name"));
+//
+//            if (passElem.HasAttribute("desktop"))
+//                newPass->SetIsDesktop(passElem.GetBool("desktop"));
+//
+//            // Append global defines only when pass does not redefine the shader
+//            if (passElem.HasAttribute("vs"))
+//            {
+//                newPass->SetVertexShader(passElem.GetAttribute("vs"));
+//                newPass->SetVertexShaderDefines(passElem.GetAttribute("vsdefines"));
+//            }
+//            else
+//            {
+//                newPass->SetVertexShader(globalVS);
+//                newPass->SetVertexShaderDefines(globalVSDefines + passElem.GetAttribute("vsdefines"));
+//            }
+//            if (passElem.HasAttribute("ps"))
+//            {
+//                newPass->SetPixelShader(passElem.GetAttribute("ps"));
+//                newPass->SetPixelShaderDefines(passElem.GetAttribute("psdefines"));
+//            }
+//            else
+//            {
+//                newPass->SetPixelShader(globalPS);
+//                newPass->SetPixelShaderDefines(globalPSDefines + passElem.GetAttribute("psdefines"));
+//            }
+//
+//            newPass->SetVertexShaderDefineExcludes(passElem.GetAttribute("vsexcludes"));
+//            newPass->SetPixelShaderDefineExcludes(passElem.GetAttribute("psexcludes"));
+//
+//            if (passElem.HasAttribute("lighting"))
+//            {
+//                String lighting = passElem.GetAttributeLower("lighting");
+//                newPass->SetLightingMode((PassLightingMode)GetStringListIndex(lighting.CString(), lightingModeNames,
+//                                                                              LIGHTING_UNLIT));
+//            }
+//
+//            if (passElem.HasAttribute("blend"))
+//            {
+//                String blend = passElem.GetAttributeLower("blend");
+//                newPass->SetBlendMode((BlendMode)GetStringListIndex(blend.CString(), blendModeNames, BLEND_REPLACE));
+//            }
+//
+//            if (passElem.HasAttribute("cull"))
+//            {
+//                String cull = passElem.GetAttributeLower("cull");
+//                newPass->SetCullMode((CullMode)GetStringListIndex(cull.CString(), cullModeNames, MAX_CULLMODES));
+//            }
+//
+//            if (passElem.HasAttribute("depthtest"))
+//            {
+//                String depthTest = passElem.GetAttributeLower("depthtest");
+//                if (depthTest == "false")
+//                    newPass->SetDepthTestMode(CMP_ALWAYS);
+//                else
+//                    newPass->SetDepthTestMode((CompareMode)GetStringListIndex(depthTest.CString(), compareModeNames, CMP_LESS));
+//            }
+//
+//            if (passElem.HasAttribute("depthwrite"))
+//                newPass->SetDepthWrite(passElem.GetBool("depthwrite"));
+//
+//            if (passElem.HasAttribute("alphatocoverage"))
+//                newPass->SetAlphaToCoverage(passElem.GetBool("alphatocoverage"));
+//        }
+//        else
+//            URHO3D_LOGERROR("Missing pass name");
+//
+//        passElem = passElem.GetNext("pass");
+//    }
+
+    return true;
+}
+
+void Technique::SetIsDesktop(bool enable)
+{
+    isDesktop_ = enable;
+}
+
+void Technique::ReleaseShaders()
+{
+//    for (vector<Pass >::iterator i = passes_.begin(); i != passes_.end(); ++i)
+//    {
+//        Pass* pass = i;
+//        if (pass)
+//            pass->ReleaseShaders();
+//    }
+    for(int i = 0 ; i < passes_.size(); i++)
+    {
+        Pass* pass = passes_[i];
+        if(pass)
+            pass->ReleaseShaders();
+    }
+}
+
+Technique* Technique::Clone(const string& cloneName) const
+{
+    Technique* ret = new Technique(context_);
+//    ret->SetIsDesktop(isDesktop_);
+//    ret->SetName(cloneName);
+//
+//    // Deep copy passes
+//    for (Vector<SharedPtr<Pass> >::ConstIterator i = passes_.Begin(); i != passes_.End(); ++i)
+//    {
+//        Pass* srcPass = i->Get();
+//        if (!srcPass)
+//            continue;
+//
+//        Pass* newPass = ret->CreatePass(srcPass->GetName());
+//        newPass->SetBlendMode(srcPass->GetBlendMode());
+//        newPass->SetDepthTestMode(srcPass->GetDepthTestMode());
+//        newPass->SetLightingMode(srcPass->GetLightingMode());
+//        newPass->SetDepthWrite(srcPass->GetDepthWrite());
+//        newPass->SetAlphaToCoverage(srcPass->GetAlphaToCoverage());
+//        newPass->SetIsDesktop(srcPass->IsDesktop());
+//        newPass->SetVertexShader(srcPass->GetVertexShader());
+//        newPass->SetPixelShader(srcPass->GetPixelShader());
+//        newPass->SetVertexShaderDefines(srcPass->GetVertexShaderDefines());
+//        newPass->SetPixelShaderDefines(srcPass->GetPixelShaderDefines());
+//        newPass->SetVertexShaderDefineExcludes(srcPass->GetVertexShaderDefineExcludes());
+//        newPass->SetPixelShaderDefineExcludes(srcPass->GetPixelShaderDefineExcludes());
+//    }
+
+    return ret;
+}
+
+Pass* Technique::CreatePass(const string& name)
+{
+    Pass* oldPass = GetPass(name);
+    if (oldPass)
+        return oldPass;
+
+//    SharedPtr<Pass> newPass(new Pass(name));
+//    unsigned passIndex = newPass->GetIndex();
+//    if (passIndex >= passes_.Size())
+//        passes_.Resize(passIndex + 1);
+//    passes_[passIndex] = newPass;
+//
+//    // Calculate memory use now
+//    SetMemoryUse((unsigned)(sizeof(Technique) + GetNumPasses() * sizeof(Pass)));
+
+//    return newPass;
+    return NULL;
+}
+
+void Technique::RemovePass(const string& name)
+{
+//    HashMap<String, unsigned>::ConstIterator i = passIndices.Find(name.ToLower());
+//    if (i == passIndices.End())
+//        return;
+//    else if (i->second_ < passes_.Size() && passes_[i->second_].Get())
+//    {
+//        passes_[i->second_].Reset();
+//        SetMemoryUse((unsigned)(sizeof(Technique) + GetNumPasses() * sizeof(Pass)));
+//    }
+}
+
+bool Technique::HasPass(const string& name) const
+{
+//    HashMap<string, unsigned>::ConstIterator i = passIndices.Find(name.ToLower());
+//    return i != passIndices.End() ? HasPass(i->second_) : false;
+    return false;
+}
+
+Pass* Technique::GetPass(const string& name) const
+{
+//    HashMap<String, unsigned>::ConstIterator i = passIndices.Find(name.ToLower());
+//    return i != passIndices.End() ? GetPass(i->second_) : nullptr;
+    return nullptr;
+}
+
+Pass* Technique::GetSupportedPass(const string& name) const
+{
+//    SN_HashMap<string, unsigned>::const_iterator i = passIndices.find(name.c_str());
+//    return i != passIndices.end() ? GetSupportedPass(i->second) : nullptr;
+    return  nullptr;
+}
+
+unsigned Technique::GetNumPasses() const
+{
+    unsigned ret = 0;
+
+//    for (Vector<SharedPtr<Pass> >::ConstIterator i = passes_.Begin(); i != passes_.End(); ++i)
+//    {
+//        if (i->Get())
+//            ++ret;
+//    }
+
+    return ret;
+}
+
+vector<string> Technique::GetPassNames() const
+{
+    vector<string> ret;
+
+//    for (vector<Pass>::iterator i = passes_.begin(); i != passes_.end(); ++i)
+//    {
+//        Pass* pass = i;
+//        if (pass)
+//            ret.push(pass->GetName());
+//    }
+
+    return ret;
+}
+
+vector<Pass*> Technique::GetPasses() const
+{
+    vector<Pass*> ret;
+
+//    for (vector<Pass* >::const_iterator i = passes_.begin(); i != passes_.end(); ++i)
+//    {
+//        Pass* pass = i;
+//        if (pass)
+//            ret.push(pass);
+//    }
+
+    return ret;
+}
+
+Technique Technique::CloneWithDefines(const string& vsDefines, const string& psDefines)
+{
+    // Return self if no actual defines
+//    if (vsDefines.Empty() && psDefines.Empty())
+//        return SharedPtr<Technique>(this);
+//
+//    Pair<StringHash, StringHash> key = MakePair(StringHash(vsDefines), StringHash(psDefines));
+//
+//    // Return existing if possible
+//    HashMap<Pair<StringHash, StringHash>, SharedPtr<Technique> >::Iterator i = cloneTechniques_.Find(key);
+//    if (i != cloneTechniques_.End())
+//        return i->second_;
+//
+//    // Set same name as the original for the clones to ensure proper serialization of the material. This should not be a problem
+//    // since the clones are never stored to the resource cache
+//    i = cloneTechniques_.Insert(MakePair(key, Clone(GetName())));
+//
+//    for (Vector<SharedPtr<Pass> >::ConstIterator j = i->second_->passes_.Begin(); j != i->second_->passes_.End(); ++j)
+//    {
+//        Pass* pass = (*j);
+//        if (!pass)
+//            continue;
+//
+//        if (!vsDefines.Empty())
+//            pass->SetVertexShaderDefines(pass->GetVertexShaderDefines() + " " + vsDefines);
+//        if (!psDefines.Empty())
+//            pass->SetPixelShaderDefines(pass->GetPixelShaderDefines() + " " + psDefines);
+//    }
+//
+//    return i->second_;
+    return NULL ;
+}
+
+unsigned Technique::GetPassIndex(const string& passName)
+{
+    // Initialize built-in pass indices on first call
+    if (passIndices.empty())
+    {
+        basePassIndex = passIndices["base"] = 0;
+        alphaPassIndex = passIndices["alpha"] = 1;
+        materialPassIndex = passIndices["material"] = 2;
+        deferredPassIndex = passIndices["deferred"] = 3;
+        lightPassIndex = passIndices["light"] = 4;
+        litBasePassIndex = passIndices["litbase"] = 5;
+        litAlphaPassIndex = passIndices["litalpha"] = 6;
+        shadowPassIndex = passIndices["shadow"] = 7;
+    }
+
+//    string nameLower = passName.ToLower();
+//    SN_HashMap<string, unsigned>::Iterator i = passIndices.find(nameLower);
+//    if (i != passIndices.end())
+//        return i->second_;
+//    else
+//    {
+//        unsigned newPassIndex = passIndices.size();
+//        passIndices[nameLower] = newPassIndex;
+//        return newPassIndex;
+//    }
 }
--- SourceCode/SkySnow/Graphics/Technique.h
+++ SourceCode/SkySnow/Graphics/Technique.h
@@ -5,7 +5,10 @@
 #pragma once
 
 #include "GraphicsDefs.h"
+#include "Common.h"
 #include <string>
+#include <vector>
+#include <Input/File.h>
 #include "Object.h"
 using namespace std;
 
@@ -26,6 +29,140 @@ public:
     virtual ~Pass();
 
     void SetBlendMode(BlendMode mode);
+    //设置选择模式覆盖。默认的选择模式是从材料中读取的。设置非法剔除模式max_cullmode以再次禁用覆盖。
+    void SetCullMode(CullMode mode);
+
+    void SetDepthTestMode(CompareMode mode);
+
+    void SetLightingMode(PassLightingMode mode);
+    /// Set depth write on/off.
+    void SetDepthWrite(bool enable);
+    /// Set alpha-to-coverage on/off.
+    void SetAlphaToCoverage(bool enable);
+    /// Set whether requires desktop level hardware.
+    void SetIsDesktop(bool enable);
+    /// Set vertex shader name.
+    void SetVertexShader(const string& name);
+    /// Set pixel shader name.
+    void SetPixelShader(const string& name);
+    /// Set vertex shader defines. Separate multiple defines with spaces.
+    void SetVertexShaderDefines(const string& defines);
+    /// Set pixel shader defines. Separate multiple defines with spaces.
+    void SetPixelShaderDefines(const string& defines);
+    /// Set vertex shader define excludes. Use to mark defines that the shader code will not recognize, to prevent compiling redundant shader variations.
+    void SetVertexShaderDefineExcludes(const string& excludes);
+    /// Set pixel shader define excludes. Use to mark defines that the shader code will not recognize, to prevent compiling redundant shader variations.
+    void SetPixelShaderDefineExcludes(const string& excludes);
+    /// Reset shader pointers.
+    void ReleaseShaders();
+    /// Mark shaders loaded this frame.
+    void MarkShadersLoaded(unsigned frameNumber);
+
+    /// Return pass name.
+    const string& GetName() const { return name_; }
+
+    /// Return pass index. This is used for optimal render-time pass queries that avoid map lookups.
+    unsigned GetIndex() const { return index_; }
+
+    /// Return blend mode.
+    BlendMode GetBlendMode() const { return blendMode_; }
+
+    /// Return culling mode override. If pass is not overriding culling mode (default), the illegal mode MAX_CULLMODES is returned.
+    CullMode GetCullMode() const { return cullMode_; }
+
+    /// Return depth compare mode.
+    CompareMode GetDepthTestMode() const { return depthTestMode_; }
+
+    /// Return pass lighting mode.
+    PassLightingMode GetLightingMode() const { return lightingMode_; }
+
+    /// Return last shaders loaded frame number.
+    unsigned GetShadersLoadedFrameNumber() const { return shadersLoadedFrameNumber_; }
+
+    /// Return depth write mode.
+    bool GetDepthWrite() const { return depthWrite_; }
+
+    /// Return alpha-to-coverage mode.
+    bool GetAlphaToCoverage() const { return alphaToCoverage_; }
+
+    /// Return whether requires desktop level hardware.
+    bool IsDesktop() const { return isDesktop_; }
+
+    /// Return vertex shader name.
+    const string& GetVertexShader() const { return vertexShaderName_; }
+
+    /// Return pixel shader name.
+    const string& GetPixelShader() const { return pixelShaderName_; }
+
+    /// Return vertex shader defines.
+    const string& GetVertexShaderDefines() const { return vertexShaderDefines_; }
+
+    /// Return pixel shader defines.
+    const string& GetPixelShaderDefines() const { return pixelShaderDefines_; }
+
+    /// Return vertex shader define excludes.
+    const string& GetVertexShaderDefineExcludes() const { return vertexShaderDefineExcludes_; }
+
+    /// Return pixel shader define excludes.
+    const string& GetPixelShaderDefineExcludes() const { return pixelShaderDefineExcludes_; }
+
+    /// Return vertex shaders.
+    vector<ShaderVariation>& GetVertexShaders() { return vertexShaders_; }
+
+    /// Return pixel shaders.
+    vector<ShaderVariation >& GetPixelShaders() { return pixelShaders_; }
+
+    /// Return vertex shaders with extra defines from the renderpath.
+    vector<ShaderVariation>& GetVertexShaders(const string& extraDefinesHash);
+    /// Return pixel shaders with extra defines from the renderpath.
+    vector<ShaderVariation>& GetPixelShaders(const string& extraDefinesHash);
+    /// Return the effective vertex shader defines, accounting for excludes. Called internally by Renderer.
+    string GetEffectiveVertexShaderDefines() const;
+    /// Return the effective pixel shader defines, accounting for excludes. Called internally by Renderer.
+    string GetEffectivePixelShaderDefines() const;
+
+private:
+    /// Pass index.
+    unsigned index_;
+    /// Blend mode.
+    BlendMode blendMode_;
+    /// Culling mode.
+    CullMode cullMode_;
+    /// Depth compare mode.
+    CompareMode depthTestMode_;
+    /// Lighting mode.
+    PassLightingMode lightingMode_;
+    /// Last shaders loaded frame number.
+    unsigned shadersLoadedFrameNumber_;
+    /// Depth write mode.
+    bool depthWrite_;
+    /// Alpha-to-coverage mode.
+    bool alphaToCoverage_;
+    /// Require desktop level hardware flag.
+    bool isDesktop_;
+    /// Vertex shader name.
+    string vertexShaderName_;
+    /// Pixel shader name.
+    string pixelShaderName_;
+    /// Vertex shader defines.
+    string vertexShaderDefines_;
+    /// Pixel shader defines.
+    string pixelShaderDefines_;
+    /// Vertex shader define excludes.
+    string vertexShaderDefineExcludes_;
+    /// Pixel shader define excludes.
+    string pixelShaderDefineExcludes_;
+    /// Vertex shaders.
+    vector<ShaderVariation> vertexShaders_;
+    /// Pixel shaders.
+    vector<ShaderVariation> pixelShaders_;
+    /// Vertex shaders with extra defines from the renderpath.
+//    HashMap<StringHash, Vector<SharedPtr<ShaderVariation> > > extraVertexShaders_;
+    SN_HashMap<string, vector<ShaderVariation> > extraVertexShaders_;
+    /// Pixel shaders with extra defines from the renderpath.
+    SN_HashMap<string, vector<ShaderVariation> > extraPixelShaders_;
+    /// Pass name.
+    string name_;
 };
 
 
@@ -34,7 +171,95 @@ class Technique:public Object
     ENGINE_OBJECT(Technique,Object);
     friend class Renderer;
 public:
+    /// Construct.
     Technique(Context* context);
+    /// Destruct.
+    virtual ~Technique() override;
+    /// Register object factory.
+    static void RegisterObject(Context* context);
 
-    virtual ~Technique();
+    /// Load resource from stream. May be called from a worker thread. Return true if successful.
+    virtual bool BeginLoad(File& source);
+
+    /// Set whether requires desktop level hardware.
+    void SetIsDesktop(bool enable);
+    /// Create a new pass.
+    Pass* CreatePass(const string& passName);
+    /// Remove a pass.
+    void RemovePass(const string& passName);
+    /// Reset shader pointers in all passes.
+    void ReleaseShaders();
+    /// Clone the technique. Passes will be deep copied to allow independent modification.
+    Technique* Clone(const string& cloneName = NULL) const;
+
+    /// Return whether requires desktop level hardware.
+    bool IsDesktop() const { return isDesktop_; }
+
+    /// Return whether technique is supported by the current hardware.
+    bool IsSupported() const { return !isDesktop_ || desktopSupport_; }
+
+    /// Return whether has a pass.
+    bool HasPass(unsigned passIndex) const { return passIndex < passes_.size() && passes_[passIndex] != nullptr; }
+
+    /// Return whether has a pass by name. This overload should not be called in time-critical rendering loops; use a pre-acquired pass index instead.
+    bool HasPass(const string& passName) const;
+
+    /// Return a pass, or null if not found.
+    Pass* GetPass(unsigned passIndex) const { return passIndex < passes_.size() ? passes_[passIndex] : nullptr; }
+
+    /// Return a pass by name, or null if not found. This overload should not be called in time-critical rendering loops; use a pre-acquired pass index instead.
+    Pass* GetPass(const string& passName) const;
+
+    /// Return a pass that is supported for rendering, or null if not found.
+    Pass* GetSupportedPass(unsigned passIndex) const
+    {
+        Pass* pass = passIndex < passes_.size() ? passes_[passIndex]: nullptr;
+        return pass && (!pass->IsDesktop() || desktopSupport_) ? pass : nullptr;
+    }
+
+    /// Return a supported pass by name. This overload should not be called in time-critical rendering loops; use a pre-acquired pass index instead.
+    Pass* GetSupportedPass(const string& passName) const;
+
+    /// Return number of passes.
+    unsigned GetNumPasses() const;
+    /// Return all pass names.
+    vector<string> GetPassNames() const;
+    /// Return all passes.
+    vector<Pass*> GetPasses() const;
+
+    /// Return a clone with added shader compilation defines. Called internally by Material.
+    Technique CloneWithDefines(const string& vsDefines, const string& psDefines);
+
+    /// Return a pass type index by name. Allocate new if not used yet.
+    static unsigned GetPassIndex(const string& passName);
+
+    /// Index for base pass. Initialized once GetPassIndex() has been called for the first time.
+    static unsigned basePassIndex;
+    /// Index for alpha pass. Initialized once GetPassIndex() has been called for the first time.
+    static unsigned alphaPassIndex;
+    /// Index for prepass material pass. Initialized once GetPassIndex() has been called for the first time.
+    static unsigned materialPassIndex;
+    /// Index for deferred G-buffer pass. Initialized once GetPassIndex() has been called for the first time.
+    static unsigned deferredPassIndex;
+    /// Index for per-pixel light pass. Initialized once GetPassIndex() has been called for the first time.
+    static unsigned lightPassIndex;
+    /// Index for lit base pass. Initialized once GetPassIndex() has been called for the first time.
+    static unsigned litBasePassIndex;
+    /// Index for lit alpha pass. Initialized once GetPassIndex() has been called for the first time.
+    static unsigned litAlphaPassIndex;
+    /// Index for shadow pass. Initialized once GetPassIndex() has been called for the first time.
+    static unsigned shadowPassIndex;
+
+private:
+    /// Require desktop GPU flag.
+    bool isDesktop_;
+    /// Cached desktop GPU support flag.
+    bool desktopSupport_;
+    /// Passes.
+    vector<Pass*> passes_;
+    /// Cached clones with added shader compilation defines.
+    SN_HashMap<std::pair<string, string>, Technique> cloneTechniques_;
+
+    /// Pass index assignments.
+    static SN_HashMap<string, unsigned> passIndices;
 };
